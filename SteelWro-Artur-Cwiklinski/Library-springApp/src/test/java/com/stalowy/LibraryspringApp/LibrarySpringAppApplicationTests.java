52467064413d29398a98c845fccb4906827156ec
stalowy <arci1910@wp.pl>
2019-03-06 22:41:23 +0100

    created empty project

---
+++ SteelWro-Artur-Cwiklinski/Library-springApp/src/test/java/com/stalowy/LibraryspringApp/LibrarySpringAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stalowy.LibraryspringApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibrarySpringAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
